5816febebab22f5d7ffb370594b0a173ea162ee2
krishnaravi12 <krishnaravi1212@gmail.com>
2025-07-14 20:46:08 +0530

    project updated

---
+++ src/components/SearchTodo.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const SearchTodo = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <div className="label form-label">Title</div>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <div className="button btn btn-success">Search</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchTodo
